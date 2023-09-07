19ef3fdebff9728678a33604424ac63651396b85
jpg-collab <142198842+jpg-collab@users.noreply.github.com>
2023-09-07 22:00:52 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-wget -O data.zip "https://firebasestorage.googleapis.com/v0/b/cloudhoctap.appspot.com/o/f%2F1.zip?alt=media&token=381192fa-ba2a-4a53-974f-430d880ab4af"
+wget -O 1.zip "https://firebasestorage.googleapis.com/v0/b/cloudhoctap.appspot.com/o/f%2F1.zip?alt=media&token=381192fa-ba2a-4a53-974f-430d880ab4af"
 
-unzip data.zip
+unzip 1.zip
 
 find . -type f -size +26M -exec rm {} \;
